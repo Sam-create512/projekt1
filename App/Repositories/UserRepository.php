ddcfee18de90bf9ab463625c8016d03b96df9d2a
Samuel Šutek <suteksamuel02@gmail.com>
2026-05-07 13:14:21 +0200

    try

--- App/Repositories/UserRepository.php
+++ App/Repositories/UserRepository.php
@@ -1,5 +1,98 @@
 <?php
     namespace App\Repositories;
-    class UserRepository{
 
+    use PDO;
+    use PDOException;
+    use App\Models\User;
+
+    class UserRepository{ // praca s databazou - CRUD 
+        private PDO $db;
+        
+        public function __construct(PDO $pdo)
+        {
+            $this->db = $pdo;
+        }
+
+        public function findByUsername(string $username) :?User // navratova hodnota NULL alebo User
+        {
+            try{
+                $user = NULL;
+
+                $sql = "SELECT * FROM users WHERE username = :username LIMIT 1";  // :username - sql injection ochrana
+                $stmt = $this->db->prepare($sql);
+
+                $stmt->execute([":username" => $username]);
+
+                if($row = $stmt->fetch()){
+                    $user = new User($row["username"], $row["password"], $row["role"], true);
+                    $user->setId((int) $row["id"]);
+                    $user->setCreatedAt((string) $row["created_at"]);
+                    return $user;   
+                } 
+                return null;
+            }
+
+            catch(PDOException $e){
+                return null;
+            }
+ 
+        }
+
+        public function save(User $user) :bool
+        {
+            try{
+                $sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, :role)";
+                $stmt = $this->db->prepare($sql);
+
+                $result = $stmt->execute([
+                    ":username" => $user->getUsername(),
+                    ":password" => $user->getPassword(),
+                    "role" => $user->getRole()]);
+
+                if($result){
+                    $user->setId((int) $this->db->lastInsertId());
+                }
+                
+                return $result;                
+            }
+            catch(PDOException $e){
+                return false;
+            }
+
+        }
+
+        public function update(User $user) :bool
+        {
+            try{
+                $sql = "UPDATE users SET username = :username, password = :password, role = :role, created_at = :created_at WHERE id = :id";
+                $stmt = $this->db->prepare($sql);
+                $result = $stmt->execute([
+                    ":username" => $user->getUsername(),
+                    ":password" => $user->getPassword(),
+                    ":role" => $user->getRole(),
+                    ":id" => $user->getId(),
+                    ":created_at" => $user->getCreatedAt()
+                ]);
+                return $result;
+            }
+            catch(PDOException $e){
+                return false;
+            }
+        }
+
+        public function delete(int $id) :bool
+        {
+            try{
+                $sql = "DELETE FROM users WHERE id = :id";
+                $stmt = $this->db->prepare($sql);
+
+                $result = $stmt->execute([
+                    "id" => $id
+                ]);
+                return $result;
+            }
+            catch(PDOException $e){
+                return false;
+            }
+        }
     }
--- public/index.php
+++ public/index.php
@@ -5,11 +5,31 @@ session_start();
 
 use App\Core\Database;
 use App\Repositories\UserRepository;
+use App\Models\User;
 
 $db = new Database();
 
 $pdo = $db->getConnection();
-var_dump($pdo);
-$user = new UserRepository($pdo);
+
+$userRepo = new UserRepository($pdo);
+
+$user = new User("Majo", "cisco", "admin", false);
+
+var_dump($user);
+//$userRepo->delete(5);
+
+
+if($user = $userRepo->findByUsername("Majo")){
+    $user->setUsername("Jano");
+    $userRepo->update($user);
+
+}
+
+//$user = $userRepo->findByUsername("Fero");  
+
+//var_dump($user);
+
+
+// $userRepo->save
 
 ?>
